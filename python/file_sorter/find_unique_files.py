4d8fb5660ea78fa0d61708e3acf6d9601af8a1a1
Dan Aukes <danaukes@gmail.com>
2020-07-10 16:12:27 -0700

    aded matched files

--- python/file_sorter/find_unique_files.py
+++ python/file_sorter/find_unique_files.py
@@ -1,115 +1,50 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 24 08:15:20 2019
+Created on Tue Jul  7 23:49:30 2020
 
 @author: danaukes
 """
 
 import file_sorter.support as fus
-import os
-import sys
+import file_sorter.images as fui
 import yaml
 
+# path1 = '/mnt/photos/2020/DCIM/Camera'
+# compare1 = fus.scan_compare_dir(path1, recursive=True,file_filter=fui.filter_img_filetype,hasher=fus.hash_filesize)
+# compare1.save('./','hash1.yaml')
+with open('hash1.yaml') as f:
+    compare1 = yaml.load(f)
 
-print('###########################')
-print('argv:')
-print(sys.argv)
+# path2 = '/mnt/photos/2020/unsorted'
+# compare2 = fus.scan_compare_dir(path2, recursive=True,file_filter=fui.filter_img_filetype,hasher=fus.hash_filesize)
+# compare2.save('./','hash2.yaml')
+with open('hash2.yaml') as f:
+    compare2 = yaml.load(f)
 
-#paths = 'Y:/2017', 'Y:/2018', 'Y:/2019'
-#fus.rebuild_compare_info(p1, recursive=True)
-#fus.rebuild_compare_info(p1, recursive=True,local_ha
-#p1 = 'C:/Users/danaukes/Dropbox (Personal)/Camera Uploads'
-#p1 = 'C:/Users/danaukes/Dropbox (ASU)/Mendeley Desktop/Archive1'
-compare_info = fus.scan_compare_dir(p1, recursive=True)
-compare_info.save('./','hash_size.yaml')
+same_size = set(compare1.hashes).intersection(set(compare2.hashes))
+
+# compare1i = fus.scan_compare_dir(path1, recursive=True,file_filter=fui.filter_img_filetype,hasher=fui.gen_p_hash_opt)
+# compare1i.save('./','hash1i.yaml')
+with open('hash1i.yaml') as f:
+    compare1i = yaml.load(f)
+# compare2i = fus.scan_compare_dir(path2, recursive=True,file_filter=fui.filter_img_filetype,hasher=fui.gen_p_hash_opt)
+# compare2i.save('./','hash2i.yaml')
+with open('hash2i.yaml') as f:
+    compare2i = yaml.load(f)
+
+
+same_look = set(compare1i.hashes).intersection(set(compare2i.hashes))
+
+# for key,value in compare2.hash_file_dict.items():
+#     if len(value)>1:
+#         print(value)
+
+dcim_filenames1 = set([item2 for item in same_size for item2 in compare1.hash_file_dict[item]])
+dcim_filenames2 = set([item2 for item in same_look for item2 in compare1i.hash_file_dict[item]])
+dcim_filenames3 = dcim_filenames1.intersection(dcim_filenames2)
+
+with open ('matched_files.yaml','w') as f:
+    yaml.dump(list(dcim_filenames3),f)
     
-with open('hash_size.yaml') as f:
-    compare_info = yaml.load(f)
-
-#source_dir = 'C:/shared/duplicate phone photos'
-#source_dir = 'C:/Users/danaukes/Desktop/duplicate phone photos'
-
-source_dirs = []
-#source_dirs.append('C:/Users/danaukes/Dropbox (ASU)/Mendeley Desktop/Archive1')
-#source_dirs.append('C:/Users/danaukes/Dropbox (ASU)/Mendeley Desktop/Archive2')
-#source_dirs.append('C:/Users/danaukes/Dropbox (ASU)/Mendeley Desktop/Archive3')
-#source_dirs.append('C:/Users/danaukes/Dropbox (ASU)/Mendeley Desktop/Archive4')
-
-original = 'original'
-#duplicate = 'duplicate'
-
-        
-compare_size_dict_rev = compare_info.hash_file_dict
-compare_size_set = compare_info.hashes
-#compare_size_dict_rev = compare_info['size_file_dict']
-#compare_size_set = compare_info['sizes']
-
-print('###########################')
-print('')
-
-#items = fus.find_items_with_matching_sizes(compare_size_dict_rev)
-#print(items)
-
-source_files = []
-
-for source_dir in source_dirs:
-    for dirpath,dirnames,filenames in os.walk(source_dir):
-        full_file_names = [os.path.join(dirpath,item) for item in filenames]
-        source_files.extend(full_file_names)
-
-source_size_dict = {}
-source_sizes  = []
-
-candidate = []
-comparison_set = []
-
-matched_files = []
-unmatched_files = []
-    
-l= len(source_files)
-
-ii = 0
-while not not source_files:
-#for ii,filename in enumerate(source_files):
-    filename=source_files.pop(0)
-#    print('{0:.0f}/{1:.0f}'.format(ii,l))
-    size = os.path.getsize(filename)
-    source_size_dict[filename] = size
-    source_sizes.append(size)
-    if size in compare_size_set:
-#        candidate.append(filename)
-        matched = False
-        for item in compare_size_dict_rev[size]:
-            if filename != item:
-                comparison_set.append((filename,item))
-                if fus.check_match(filename,item):
-                    matched = True
-                    break
-        if matched:
-            matched_files.append((filename,item))
-        else:
-            unmatched_files.append(filename)
-    else:
-        unmatched_files.append(filename)
-    print(ii,len(source_files),l,len(matched_files),len(unmatched_files))
-    if ii%10==0:
-        fus.save_progress(source_files,matched_files,unmatched_files)
-    ii+=1
-
-
-fus.save_progress(source_files,matched_files,unmatched_files)
-
-#
-#            for a,b in f1.readlines(), f2.readlines():
-#            a=fa.readline()
-#            b=fb.readline()
-#   
-#results = []         
-#for item in comparison_set:
-##    print(item)
-#    results.append(compare_file_data(*item))
-#        
-#    
-#print('###########################')
-#print('unmatched files')
-#print(unmatched)
+not_matched = dcim_filenames1.symmetric_difference(dcim_filenames2)
+print(not_matched)
