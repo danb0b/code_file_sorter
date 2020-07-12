b7b8abac3c398edf3af1f78a73633cfed0b5a555
Dan Aukes <danaukes@gmail.com>
2020-07-12 14:48:27 -0700

    added sha256 hashing

--- python/file_sorter/support.py
+++ python/file_sorter/support.py
@@ -97,6 +97,22 @@ def hash_filesize(filename):
     size = os.path.getsize(filename)
     return size
 
+def hash_file(filename):
+    import hashlib
+    
+    # file = ".\myfile.txt" # Location of the file (can be set a different way)
+    BLOCK_SIZE = 65536 # The size of each read from the file
+    
+    file_hash = hashlib.sha256() # Create the hash object, can use something other than `.sha256()` if you wish
+    with open(filename, 'rb') as f: # Open the file to read it's bytes
+        fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+        while len(fb) > 0: # While there is still data being read from the file
+            file_hash.update(fb) # Update the hash
+            fb = f.read(BLOCK_SIZE) # Read the next block from the file
+    
+    return file_hash.hexdigest() # Get the hexadecimal digest of the hash
+    
+    
 def check_match(filea,fileb):
     with open(filea,'rb') as  fa:
         with open(fileb,'rb') as  fb:
